f049ca698dd5f8a1c5f9f4f3409ca74928e8e619
Nicoguid <78450806+Nicoguid@users.noreply.github.com>
2024-04-03 14:46:41 -0300

    ejercicio 10 corregido

--- TP3.py
+++ TP3.py
@@ -8,9 +8,9 @@ elif estado_civil == 2:
     importe = sueldo_basico + sueldo_basico*0.07*antiguedad
 else:
     print("Solo puede ingresar 1 o 2")
-jubilacion = importe - importe*0.11
-obra_social = importe - importe*0.03
-sindicato = importe - importe*0.03
+jubilacion = importe*0.11
+obra_social = importe*0.03
+sindicato = importe*0.03
 sueldo_neto = importe - jubilacion - obra_social - sindicato
-print("Sueldo basico: ", sueldo_basico "Antiguedad: ", antiguedad "Estado civil: ", estado_civil "Descuento jubilacion: ", jubilacion "Descuento obra social: ", obra_social "Descuento sindicato: ", sindicato)
+print("Sueldo basico: ", sueldo_basico, "\nAntiguedad: ", antiguedad, "\nEstado civil: ", estado_civil, "\nDescuento jubilacion: ", jubilacion, "\nDescuento obra social: ", obra_social, "\nDescuento sindicato: ", sindicato)
 print("Su sueldo neto es: ", sueldo_neto)
